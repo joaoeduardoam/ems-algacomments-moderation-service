bd28d67b58b5df9b2e66cdf3c78ff4bbc7ec9315
João Eduardo Araújo de Meneses <jeduardo.meneses@gmail.com>
2025-04-24 10:32:00 -0300

    Initial projects

---
+++ src/test/java/com/joaoeduardo/algacomments/moderation/service/ModerationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaoeduardo.algacomments.moderation.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModerationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
